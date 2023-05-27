febcc0c75d7630491302032710ca7d79add9cd23
michalm57 <mroczekmichal57@gmail.com>
2023-05-27 10:36:20 +0200

    refactor AuthController

--- Modules/Users/Http/Controllers/AuthController.php
+++ Modules/Users/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace Modules\Users\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Modules\Users\Services\AuthService;
+use Modules\Users\Services\UserService;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,16 +12,21 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthController extends Controller
 {
     /**
-     * @var AuthService
+     * @var UserService
      */
-    protected AuthService $authService;
+    protected UserService $service;
 
-    public function __construct(AuthService $service)
+    /**
+     * Create a new UserController instance.
+     *
+     * @param UserService $service The UserService instance.
+     * @return void
+     */
+    public function __construct(UserService $service)
     {
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
-        $this->authService = $service;
+        $this->service = $service;
     }
-
 
     /**
      * Registring new user to a system.
@@ -29,24 +34,24 @@ class AuthController extends Controller
      * @param Request $request
      * @return JsonResponse
      */
-    public function register(Request $request) : JsonResponse
+    public function register(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed|min:6',
         ]);
-    
+
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), RESPONSE::HTTP_BAD_REQUEST);
         };
-    
+
         $userData = array_merge(
             $validator->validated(),
             ['password' => bcrypt($request->password)],
         );
-    
-        $user = $this->authService->createUser($userData);
+
+        $user = $this->service->createUser($userData);
 
         return response()->json([
             'message' => 'User successfully registered',
@@ -54,6 +59,12 @@ class AuthController extends Controller
         ], RESPONSE::HTTP_OK);
     }
 
+    /**
+     * Login user to a system.
+     * 
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -65,7 +76,7 @@ class AuthController extends Controller
             return response()->json($validator->errors()->toJson(), RESPONSE::HTTP_UNPROCESSABLE_ENTITY);
         };
 
-        if (!$token = auth()->attempt($validator->validated())){
+        if (!$token = auth()->attempt($validator->validated())) {
             return response()->json(['error', 'Unauthorized'], RESPONSE::HTTP_UNAUTHORIZED);
         }
 
@@ -77,11 +88,21 @@ class AuthController extends Controller
         ]);
     }
 
+    /**
+     * Return user data if authenticated.
+     * 
+     * @return JsonResponse
+     */
     public function user()
     {
         return response()->json(auth()->user());
     }
 
+    /**
+     * Logout user.
+     * 
+     * @return JsonResponse
+     */
     public function logout()
     {
         auth()->logout();
