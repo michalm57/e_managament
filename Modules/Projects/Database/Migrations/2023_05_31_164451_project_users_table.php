ea333c6eafcb1f3d86af1c03bea93af09039161e
michalm57 <mroczekmichal57@gmail.com>
2023-05-31 19:41:01 +0200

    create project_users table and model ProjectUser

---
+++ Modules/Projects/Database/Migrations/2023_05_31_164451_project_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_users');
+    }
+};
---
+++ Modules/Projects/Entities/ProjectUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Projects\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectUser extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'project_users';
+}
