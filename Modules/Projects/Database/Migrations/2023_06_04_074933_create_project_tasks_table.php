3ed74a4a3753a44e84961148f96bbb23f51e7de2
michalm57 <mroczekmichal57@gmail.com>
2023-06-04 10:11:12 +0200

    create project task table

---
+++ Modules/Projects/Database/Migrations/2023_06_04_074933_create_project_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->unsignedBigInteger('task_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('task_id')->references('id')->on('tasks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_tasks');
+    }
+};
