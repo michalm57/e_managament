a96b3c12def09c927916e8c0eca7edc43ede8199
michalm57 <mroczekmichal57@gmail.com>
2023-06-05 20:24:39 +0200

    create base project resource

---
+++ Modules/Projects/Resources/ProjectResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\Projects\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
